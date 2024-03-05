3044f27c1118fbb47c6ab365fc2739cd28a3f6d3
oforduhh <activation340@gmail.com>
2024-03-05 13:58:09 +0100

    how to reverse a string

---
+++ reversingAstring.ts
@@ -0,0 +1,8 @@
+const namez = "joseph";
+const array: string[] = namez.split("");
+let newArray: string[] = [];
+newArray = array.reverse();
+
+console.log(newArray);
+const inverseArray = newArray.join("");
+console.log(inverseArray);
